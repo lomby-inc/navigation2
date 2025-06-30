abf74cc0a9f90b823e98feee2053a239d9129898
Scott Duncan <scott.duncan@lomby.jp>
2025-06-30 14:56:29 +0900

    Nav2Autoware Add publisher for optimal trajectory as path
    Added a simple trajectory path publisher so we can convert it for autoware
    30/06/25

--- nav2_mppi_controller/include/nav2_mppi_controller/tools/trajectory_visualizer.hpp
+++ nav2_mppi_controller/include/nav2_mppi_controller/tools/trajectory_visualizer.hpp
@@ -1,4 +1,5 @@
-// Copyright (c) 2022 Samsung Research America, @artofnothingness Alexey Budyakov
+// Copyright (c) 2022 Samsung Research America, @artofnothingness Alexey
+// Budyakov
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -24,84 +25,92 @@
 #include "rclcpp_lifecycle/lifecycle_node.hpp"
 #include "tf2_geometry_msgs/tf2_geometry_msgs.hpp"
 
+#include "nav2_mppi_controller/models/trajectories.hpp"
 #include "nav2_mppi_controller/tools/parameters_handler.hpp"
 #include "nav2_mppi_controller/tools/utils.hpp"
-#include "nav2_mppi_controller/models/trajectories.hpp"
 
-namespace mppi
-{
+namespace mppi {
 
 /**
  * @class mppi::TrajectoryVisualizer
  * @brief Visualizes trajectories for debugging
  */
-class TrajectoryVisualizer
-{
+class TrajectoryVisualizer {
 public:
   /**
-    * @brief Constructor for mppi::TrajectoryVisualizer
-    */
+   * @brief Constructor for mppi::TrajectoryVisualizer
+   */
   TrajectoryVisualizer() = default;
 
   /**
-    * @brief Configure trajectory visualizer
-    * @param parent WeakPtr to node
-    * @param name Name of plugin
-    * @param frame_id Frame to publish trajectories in
-    * @param dynamic_parameter_handler Parameter handler object
-    */
-  void on_configure(
-    rclcpp_lifecycle::LifecycleNode::WeakPtr parent, const std::string & name,
-    const std::string & frame_id, ParametersHandler * parameters_handler);
+   * @brief Configure trajectory visualizer
+   * @param parent WeakPtr to node
+   * @param name Name of plugin
+   * @param frame_id Frame to publish trajectories in
+   * @param dynamic_parameter_handler Parameter handler object
+   */
+  void on_configure(rclcpp_lifecycle::LifecycleNode::WeakPtr parent,
+                    const std::string &name, const std::string &frame_id,
+                    ParametersHandler *parameters_handler);
 
   /**
-    * @brief Cleanup object on shutdown
-    */
+   * @brief Cleanup object on shutdown
+   */
   void on_cleanup();
 
   /**
-    * @brief Activate object
-    */
+   * @brief Activate object
+   */
   void on_activate();
 
   /**
-    * @brief Deactivate object
-    */
+   * @brief Deactivate object
+   */
   void on_deactivate();
 
   /**
-    * @brief Add an optimal trajectory to visualize
-    * @param trajectory Optimal trajectory
-    */
-  void add(const xt::xtensor<float, 2> & trajectory, const std::string & marker_namespace);
+   * @brief Add an optimal trajectory to visualize
+   * @param trajectory Optimal trajectory
+   */
+  void add(const xt::xtensor<float, 2> &trajectory,
+           const std::string &marker_namespace);
 
   /**
-    * @brief Add candidate trajectories to visualize
-    * @param trajectories Candidate trajectories
-    */
-  void add(const models::Trajectories & trajectories, const std::string & marker_namespace);
+   * @brief Add candidate trajectories to visualize
+   * @param trajectories Candidate trajectories
+   */
+  void add(const models::Trajectories &trajectories,
+           const std::string &marker_namespace);
 
   /**
-    * @brief Visualize the plan
-    * @param plan Plan to visualize
-    */
-  void visualize(const nav_msgs::msg::Path & plan);
+   * @brief Visualize the plan
+   * @param plan Plan to visualize
+   */
+  void visualize(const nav_msgs::msg::Path &plan);
 
   /**
-    * @brief Reset object
-    */
+   * @brief Reset object
+   */
   void reset();
 
 protected:
   std::string frame_id_;
-  std::shared_ptr<rclcpp_lifecycle::LifecyclePublisher<visualization_msgs::msg::MarkerArray>>
-  trajectories_publisher_;
-  std::shared_ptr<rclcpp_lifecycle::LifecyclePublisher<nav_msgs::msg::Path>> transformed_path_pub_;
+  std::shared_ptr<rclcpp_lifecycle::LifecyclePublisher<
+      visualization_msgs::msg::MarkerArray>>
+      trajectories_publisher_;
+  std::shared_ptr<rclcpp_lifecycle::LifecyclePublisher<nav_msgs::msg::Path>>
+      transformed_path_pub_;
+  std::shared_ptr<rclcpp_lifecycle::LifecyclePublisher<nav_msgs::msg::Path>>
+      trajectory_path_pub_;
 
+  rclcpp::Time plan_time_;
+
+  std::shared_ptr<nav_msgs::msg::Path> traj_plan_;
   std::unique_ptr<visualization_msgs::msg::MarkerArray> points_;
+  rclcpp_lifecycle::LifecycleNode::WeakPtr node_;
   int marker_id_ = 0;
 
-  ParametersHandler * parameters_handler_;
+  ParametersHandler *parameters_handler_;
 
   size_t trajectory_step_{0};
   size_t time_step_{0};
@@ -109,6 +118,6 @@ protected:
   rclcpp::Logger logger_{rclcpp::get_logger("MPPIController")};
 };
 
-}  // namespace mppi
+} // namespace mppi
 
-#endif  // NAV2_MPPI_CONTROLLER__TOOLS__TRAJECTORY_VISUALIZER_HPP_
+#endif // NAV2_MPPI_CONTROLLER__TOOLS__TRAJECTORY_VISUALIZER_HPP_
--- nav2_mppi_controller/src/trajectory_visualizer.cpp
+++ nav2_mppi_controller/src/trajectory_visualizer.cpp
@@ -1,4 +1,5 @@
-// Copyright (c) 2022 Samsung Research America, @artofnothingness Alexey Budyakov
+// Copyright (c) 2022 Samsung Research America, @artofnothingness Alexey
+// Budyakov
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -12,83 +13,94 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include <memory>
 #include "nav2_mppi_controller/tools/trajectory_visualizer.hpp"
+#include <memory>
 
-namespace mppi
-{
+namespace mppi {
 
 void TrajectoryVisualizer::on_configure(
-  rclcpp_lifecycle::LifecycleNode::WeakPtr parent, const std::string & name,
-  const std::string & frame_id, ParametersHandler * parameters_handler)
-{
+    rclcpp_lifecycle::LifecycleNode::WeakPtr parent, const std::string &name,
+    const std::string &frame_id, ParametersHandler *parameters_handler) {
   auto node = parent.lock();
+  traj_plan_ = std::make_shared<nav_msgs::msg::Path>();
+  plan_time_ = node->get_clock()->now();
   logger_ = node->get_logger();
   frame_id_ = frame_id;
   trajectories_publisher_ =
-    node->create_publisher<visualization_msgs::msg::MarkerArray>("/trajectories", 1);
-  transformed_path_pub_ = node->create_publisher<nav_msgs::msg::Path>("transformed_global_plan", 1);
+      node->create_publisher<visualization_msgs::msg::MarkerArray>(
+          "/trajectories", 1);
+  trajectory_path_pub_ =
+      node->create_publisher<nav_msgs::msg::Path>("mppi_trajectory_path", 1);
+  transformed_path_pub_ =
+      node->create_publisher<nav_msgs::msg::Path>("transformed_global_plan", 1);
   parameters_handler_ = parameters_handler;
 
-  auto getParam = parameters_handler->getParamGetter(name + ".TrajectoryVisualizer");
+  auto getParam =
+      parameters_handler->getParamGetter(name + ".TrajectoryVisualizer");
 
   getParam(trajectory_step_, "trajectory_step", 5);
   getParam(time_step_, "time_step", 3);
+  node_ = node;
 
   reset();
 }
 
-void TrajectoryVisualizer::on_cleanup()
-{
+void TrajectoryVisualizer::on_cleanup() {
   trajectories_publisher_.reset();
   transformed_path_pub_.reset();
+  trajectory_path_pub_.reset();
 }
 
-void TrajectoryVisualizer::on_activate()
-{
+void TrajectoryVisualizer::on_activate() {
   trajectories_publisher_->on_activate();
   transformed_path_pub_->on_activate();
+  trajectory_path_pub_->on_activate();
 }
 
-void TrajectoryVisualizer::on_deactivate()
-{
+void TrajectoryVisualizer::on_deactivate() {
   trajectories_publisher_->on_deactivate();
   transformed_path_pub_->on_deactivate();
+  trajectory_path_pub_->on_deactivate();
 }
 
-void TrajectoryVisualizer::add(
-  const xt::xtensor<float, 2> & trajectory, const std::string & marker_namespace)
-{
-  auto & size = trajectory.shape()[0];
+void TrajectoryVisualizer::add(const xt::xtensor<float, 2> &trajectory,
+                               const std::string &marker_namespace) {
+  auto &size = trajectory.shape()[0];
   if (!size) {
     return;
   }
 
   auto add_marker = [&](auto i) {
-      float component = static_cast<float>(i) / static_cast<float>(size);
+    float component = static_cast<float>(i) / static_cast<float>(size);
 
-      auto pose = utils::createPose(trajectory(i, 0), trajectory(i, 1), 0.06);
-      auto scale =
-        i != size - 1 ?
-        utils::createScale(0.03, 0.03, 0.07) :
-        utils::createScale(0.07, 0.07, 0.09);
-      auto color = utils::createColor(0, component, component, 1);
-      auto marker = utils::createMarker(
-        marker_id_++, pose, scale, color, frame_id_, marker_namespace);
-      points_->markers.push_back(marker);
-    };
+    auto pose = utils::createPose(trajectory(i, 0), trajectory(i, 1), 0.06);
+    auto node = node_.lock();
+    auto logger = node->get_logger();
+    //    RCLCPP_INFO(logger, "In marker add");
+    geometry_msgs::msg::PoseStamped pose_stamped;
+    pose_stamped.pose = pose;
+    pose_stamped.header.stamp = plan_time_;
+    pose_stamped.header.frame_id = "base_link";
+    traj_plan_->poses.push_back(pose_stamped);
+    auto scale = i != size - 1 ? utils::createScale(0.03, 0.03, 0.07)
+                               : utils::createScale(0.07, 0.07, 0.09);
+    auto color = utils::createColor(0, component, component, 1);
+    auto marker = utils::createMarker(marker_id_++, pose, scale, color,
+                                      frame_id_, marker_namespace);
+    points_->markers.push_back(marker);
+  };
 
   for (size_t i = 0; i < size; i++) {
     add_marker(i);
   }
 }
 
-void TrajectoryVisualizer::add(
-  const models::Trajectories & trajectories, const std::string & marker_namespace)
-{
-  auto & shape = trajectories.x.shape();
+void TrajectoryVisualizer::add(const models::Trajectories &trajectories,
+                               const std::string &marker_namespace) {
+  auto &shape = trajectories.x.shape();
   const float shape_1 = static_cast<float>(shape[1]);
-  points_->markers.reserve(floor(shape[0] / trajectory_step_) * floor(shape[1] * time_step_));
+  points_->markers.reserve(floor(shape[0] / trajectory_step_) *
+                           floor(shape[1] * time_step_));
 
   for (size_t i = 0; i < shape[0]; i += trajectory_step_) {
     for (size_t j = 0; j < shape[1]; j += time_step_) {
@@ -96,35 +108,43 @@ void TrajectoryVisualizer::add(
       float blue_component = 1.0f - j_flt / shape_1;
       float green_component = j_flt / shape_1;
 
-      auto pose = utils::createPose(trajectories.x(i, j), trajectories.y(i, j), 0.03);
+      auto pose =
+          utils::createPose(trajectories.x(i, j), trajectories.y(i, j), 0.03);
       auto scale = utils::createScale(0.03, 0.03, 0.03);
       auto color = utils::createColor(0, green_component, blue_component, 1);
-      auto marker = utils::createMarker(
-        marker_id_++, pose, scale, color, frame_id_, marker_namespace);
+      auto marker = utils::createMarker(marker_id_++, pose, scale, color,
+                                        frame_id_, marker_namespace);
 
       points_->markers.push_back(marker);
     }
   }
 }
 
-void TrajectoryVisualizer::reset()
-{
+void TrajectoryVisualizer::reset() {
   marker_id_ = 0;
   points_ = std::make_unique<visualization_msgs::msg::MarkerArray>();
+  traj_plan_ = std::make_shared<nav_msgs::msg::Path>();
+
+  auto node = node_.lock();
+  plan_time_ = node->get_clock()->now();
+  traj_plan_->header.stamp = plan_time_;
 }
 
-void TrajectoryVisualizer::visualize(const nav_msgs::msg::Path & plan)
-{
+void TrajectoryVisualizer::visualize(const nav_msgs::msg::Path &plan) {
   if (trajectories_publisher_->get_subscription_count() > 0) {
     trajectories_publisher_->publish(std::move(points_));
   }
-
-  reset();
 
   if (transformed_path_pub_->get_subscription_count() > 0) {
     auto plan_ptr = std::make_unique<nav_msgs::msg::Path>(plan);
     transformed_path_pub_->publish(std::move(plan_ptr));
   }
+
+  if (trajectory_path_pub_->get_subscription_count() > 0) {
+    trajectory_path_pub_->publish(*traj_plan_);
+  }
+
+  reset();
 }
 
-}  // namespace mppi
+} // namespace mppi
